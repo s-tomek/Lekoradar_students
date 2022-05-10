8621f28807512a5b1f7187b6e8d8bd59f64059fa
Krzaq8 <kubakropkazet@gmail.com>
2022-05-10 12:02:20 +0200

    zmiana linków i stare style

--- index.py
+++ index.py
@@ -4,9 +4,7 @@ from flask import Flask,\
                   request,\
                   flash,\
                   redirect
-import psycopg2
 
-import sql_scripts
 from utils import *
 from datetime import date
 
@@ -14,14 +12,18 @@ app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = "2b3f12f3ef12a6c86b"
 
 
-
-
-@app.route('/admin/', methods=['GET', 'POST'])
+@app.route('/admin')
 def admin_home():
    substance_list = ['a', 'b', 'c', 'd']
    return render_template("home.html", substances=substance_list)
    # return render_template("test.html")
 
+@app.route("/results")
+def results():
+    # TODO tutaj wstawiam funkcję od Asi, która zwraca tabelę wynikową dla konkretnej substancji aktywnej
+    # result_table = get_result_table(request.args.get('sub'))
+    result_table = [[request.args.get('subs'), "h2", "h3", "h4"], ["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i", "j", "k", "l"]]
+    return render_template("results.html", table=result_table)
 
 if __name__ == "__main__":
    app.run(debug=True)
