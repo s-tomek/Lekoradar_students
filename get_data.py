9441e52b06a65ddef5d30e0ab129d5dedc543c8a
Krzaq8 <kubakropkazet@gmail.com>
2022-05-10 14:44:55 +0200

    naprawiono sortowanie i substancje złożone

--- get_data.py
+++ get_data.py
@@ -1,27 +1,6 @@
-from database.connection import connection
-
-query = '''SELECT 
-  M.id, 
-  M.name, 
-  I.form, 
-  I.dose, 
-  M.quantity, 
-  M.id_code, 
-  M.refund_scope, 
-  M.refund, 
-  M.surcharge, 
-  I.id
-FROM 
-  Medicine M, 
-  Ingredient I, 
-  Active_substance A
-WHERE 
-  I.id = M.ingredient
-  AND I.active_substance = A.id
-  AND A.name LIKE \'{}\'
-ORDER BY 
-  I.id
-  '''
+from operator import and_
+from database.connection import connection, engine
+from sqlalchemy import MetaData, select
 
 count_no = 0
 ingr_no = 9
@@ -32,38 +11,59 @@ result_heading = ['Numer grupy', 'Nazwa', 'Postać', 'Dawka', 'Zawartość opako
   'Numer GTIN lub inny kod jednoznacznie identyfikujący produkt', 'Zakres wskazań objętych refundacją',
   'Poziom odpłatności', 'Wysokość dopłaty świadczeniobiorcy']
 
+meta = MetaData(bind=engine)
+meta.reflect()
+A = meta.tables['active_substance']
+I = meta.tables['ingredient']
+M = meta.tables['medicine']
+
 def get_result_table(sub):
-    with connection() as con:
-        data_tuples = con.execute(query.format(sub)).fetchall()
-        
-        if data_tuples == []:
-            return [result_heading]
+  query = select(M.c.id, M.c.name, I.c.form, I.c.dose, M.c.quantity, M.c.id_code, M.c.refund_scope, M.c.refund, M.c.surcharge, I.c.id)\
+    .where(M.c.ingredient == I.c.id, I.c.active_substance == A.c.id, A.c.id == sub)\
+    .order_by(I.c.id)
 
-        data = [list(i) for i in data_tuples]
-        group_start = 0
-        group_end = 1
-        count = 1
-        data[0][count_no] = count
-        prev_ingr = data[0][ingr_no]
+  data_tuples = []
+  with connection() as con:
+    data_tuples = con.execute(query).fetchall()
+    
+  if data_tuples == []:
+    return [result_heading]
 
-        for row in data[1:]:
-            
-            if row[ingr_no] != prev_ingr:
-                # zmiana grupy odpowiedników
-                count += 1
-                # posortowanie grupy odpowiedników
-                data[group_start:group_end].sort(key=lambda med: med[surcharge_no]/med[quantity_no])
-                group_start = group_end
-                prev_ingr = row[ingr_no]
-            row[count_no] = count
-            group_end += 1
+  data = [list(i) for i in data_tuples]
+  data_c = []
+  group_start = 0
+  group_end = 1
+  count = 1
+  data[0][count_no] = count
+  prev_ingr = data[0][ingr_no]
 
-        data[group_start:group_end].sort(key=lambda med: med[surcharge_no]/med[quantity_no])
+  for row in data[1:]:
+    if row[ingr_no] != prev_ingr:
+      # zmiana grupy odpowiedników
+      count += 1
+      # posortowanie grupy odpowiedników
+      data_part = data[group_start:group_end]
+      data_part.sort(key=lambda med: (float(med[surcharge_no]) / float(med[quantity_no])))
+      data_c = data_c + data_part
+      group_start = group_end
+      prev_ingr = row[ingr_no]
 
-        return [result_heading] + [i[:-1] for i in data]
+    row[count_no] = count
+    group_end += 1
+
+  data_part = data[group_start:group_end]
+  data_part.sort(key=lambda med: (float(med[surcharge_no]) / float(med[quantity_no])))
+  data_c = data_c + data_part
+  
+  return [result_heading] + [i[:-1] for i in data_c]
 
 def get_substance_table():
-    with connection() as con:
-        query_result = con.execute('''SELECT name FROM Active_substance''').fetchall()
-        return [list(substance)[0] for substance in query_result]
+  with connection() as con:
+    query_result = con.execute(select(A)).fetchall()
+    return list(query_result)
 
+def get_substance_name(id):
+  with connection() as con:
+    query_result = con.execute(select(A.c.name).where(A.c.id == id)).fetchall()
+    (name,) = list(query_result)[0]
+    return name
--- index.py
+++ index.py
@@ -5,8 +5,7 @@ from flask import Flask,\
                   flash,\
                   redirect
 
-from datetime import date
-from get_data import get_substance_table, get_result_table
+from get_data import get_substance_table, get_result_table, get_substance_name
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = "2b3f12f3ef12a6c86b"
@@ -22,10 +21,11 @@ def admin_home():
 @app.route("/results")
 def results():
     # TODO tutaj wstawiam funkcję od Asi, która zwraca tabelę wynikową dla konkretnej substancji aktywnej
-    substance = request.args.get('sub')
-    result_table = get_result_table(substance)
+    sub_id = request.args.get('sub')
+    result_table = get_result_table(sub_id)
+    sub_name = get_substance_name(sub_id)
     # result_table = [["h1", "h2", "h3", "h4"], ["a", "b", "c", "d"], ["e", "f", "g", "h"], ["i", "j", "k", "l"]]
-    return render_template("results.html", table=result_table, sub=substance)
+    return render_template("results.html", table=result_table, sub_name=sub_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
